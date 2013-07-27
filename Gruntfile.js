53910d22a4c298be0a683f4617d4670cb5599c32
T. Budiman <tbudiman@gmail.com>
2013-07-27 21:02:22 +0700

    chore(gruntfile): add jenkins integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,26 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-    // define the files to lint
-      files: ['Gruntfile.js', 'bin/**/*.js', 'test/**/*.js'],
+      // define the files to lint
+      files: ['Gruntfile.js', 'ssg.js', 'bin/**/*.js', 'test/**/*.js'],
       // configure JSHint (documented at http://www.jshint.com/docs/)
       options: {
-    // more options here if you want to override JSHint defaults
-      globals: {
-        jQuery: true,
-        console: true,
-        module: true
+        // more options here if you want to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      },
+      ci: {
+        options: {
+          jshintrc: '.jshintrc',
+          force: true,
+          reporter: 'checkstyle',
+          reporterOutput: 'results/jshint-result.xml'
+        },
+        src: ['Gruntfile.js', 'ssg.js', 'bin/**/*.js', 'test/**/*.js']
       }
-    }
   },
     simplemocha: {
     options: {
